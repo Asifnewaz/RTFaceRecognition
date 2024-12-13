6508eddb49b7fc0f6d86439939bf76e481c94d2a
Asif Newaz <asifnewaz.cste@gmail.com>
2024-12-13 16:41:55 +0100

    comment removed from face_recog

--- face_recog.py
+++ face_recog.py
@@ -64,9 +64,6 @@ while True:
         encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
     frame_count += 1
 
-    faceCurFrame = face_recognition.face_locations(imgS, model="cnn")
-    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
-
     imgBackground[162:162 + 480, 55:55 + 640] = img
     imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
 
@@ -104,7 +101,8 @@ while True:
                 # Get the Image from the storage
                 # blob = bucket.get_blob(f'Images/{id}.png')
                 # array = np.frombuffer(blob.download_as_string(), np.uint8)
-                # imgStudent = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+                imageName= "Images/" + str(id) + ".png"
+                imgStudent = cv2.imread(imageName)
                 # Update data of attendance
                 datetimeObject = datetime.strptime(studentInfo['last_attendance_time'],
                                                    "%Y-%m-%d %H:%M:%S")
@@ -146,11 +144,11 @@ while True:
                     cv2.putText(imgBackground, str(studentInfo['name']), (808 + offset, 445),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
 
-                    # if imgStudent is not None:
-                    #     imgStudentResized = cv2.resize(imgStudent, (216, 216))
-                    #     imgBackground[175:175 + 216, 909:909 + 216] = imgStudentResized
-                    # else:
-                    #     print("Warning: imgStudent is None.")
+                    if imgStudent is not None:
+                        imgStudentResized = cv2.resize(imgStudent, (216, 216))
+                        imgBackground[175:175 + 216, 909:909 + 216] = imgStudentResized
+                    else:
+                        print("Warning: imgStudent is None.")
 
                 counter += 1
 
@@ -166,3 +164,107 @@ while True:
     # cv2.imshow("Webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
+
+# #
+# import os
+# import pickle
+# import numpy as np
+# import cv2
+# import face_recognition
+# import cvzone
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import db
+# from firebase_admin import storage
+# import numpy as np
+# from datetime import datetime
+#
+# cred = credentials.Certificate("serviceAccountKey2.json")
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': "https://faceattendancerealtime-9e847-default-rtdb.firebaseio.com/",
+#     'storageBucket': "faceattendancerealtime-9e847.firebasestorage.app"
+# })
+#
+# # bucket = storage.bucket()
+#
+#
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 640)
+# cap.set(4, 480)
+#
+# imgBackground = cv2.imread('Resources/background.png')
+#
+# # Importing the mode images into a list
+# folderModePath = 'Resources/Modes'
+# modePathList = os.listdir(folderModePath)
+# imgModeList = []
+# for path in modePathList:
+#     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# # print(len(imgModeList))
+#
+# # Load the encoding file
+# print("Loading Encode File ...")
+# file = open('EncodeFile.p', 'rb')
+# encodeListKnownWithIds = pickle.load(file)
+# file.close()
+# encodeListKnown, studentIds = encodeListKnownWithIds
+# # print(studentIds)
+# print("Encode File Loaded")
+#
+# modeType = 0
+# counter = 0
+# id = -1
+# imgStudent = []
+# frame_skip = 5
+# frame_count = 0
+#
+# while True:
+#     success, img = cap.read()
+#     if not success:
+#         print("Error: Failed to read frame from webcam.")
+#         continue
+#
+#     imgS = cv2.resize(img, (160, 120))  # Directly resize to the smaller size
+#     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+#
+#     faceCurFrame = face_recognition.face_locations(imgS, model="cnn")
+#     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+#
+#     imgBackground[162:162 + 480, 55:55 + 640] = img
+#     imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+#
+#     if faceCurFrame:
+#         for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+#             matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+#             faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+#
+#             matchIndex = np.argmin(faceDis)
+#             # print("Match Index", matchIndex)
+#
+#             if matches[matchIndex]:
+#                 print("Known Face Detected")
+#                 print(studentIds[matchIndex])
+#                 y1, x2, y2, x1 = faceLoc
+#                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+#                 bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+#                 imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+#                 id = studentIds[matchIndex]
+#                 print(id)
+#                 # Display text (e.g., 'Face Detected') at the top-left corner of the face
+#                 text = "Face Detected"
+#                 font_scale = 0.6
+#                 font_color = (0, 255, 0)  # Green color
+#                 font_thickness = 1
+#                 font = cv2.FONT_HERSHEY_SIMPLEX
+#
+#                 # Put the text on the frame without background
+#                 cv2.putText(imgBackground, text, (x1, y1), font, font_scale, font_color, font_thickness,
+#                             cv2.LINE_AA)
+#
+#
+#     else:
+#         modeType = 0
+#         counter = 0
+#     # cv2.imshow("Webcam", img)
+#     cv2.imshow("Face Attendance", imgBackground)
+#     cv2.waitKey(1)
